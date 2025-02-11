b180a21182fdf85b168980f5819bcdfc8f0a815e
ThomasHaugstad <thomasgrannes@hotmail.no>
2025-02-11 12:38:28 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+import javax.swing.JFrame;
+
+public class drawpanel extends JPanel{
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+
+    }
+    
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+
+
+
+
+
+
+}
